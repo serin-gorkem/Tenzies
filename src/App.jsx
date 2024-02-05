f7fbee12e856a34e511491814ddbe503401e11a6
Crehera <92372815+serin-gorkem@users.noreply.github.com>
2024-02-05 17:10:23 +0300

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,108 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Dice from "./Components/Dice";
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 export default function App() {
-  
+  const [dice, setDice] = useState(allNewDice());
+  const [tenzies, setTenzies] = useState(false);
+  const [times, setTimes] = useState(0);
+  const bestTime = localStorage.getItem("bestTime") ? localStorage.getItem("bestTime") : "0";
+
+  useEffect(() => {
+    const allHeld = dice.every((d) => d.isHeld);
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every((d) => d.value === firstValue);
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+    } else {
+      setTenzies(false);
+    }
+  }, [dice, tenzies]);
+
+  useEffect(() => {
+    if(tenzies){
+      if(localStorage.getItem("bestTime")){
+        if(bestTime > times){
+          localStorage.setItem("bestTime", times);
+        }
+      }else{
+        localStorage.setItem("bestTime",times);
+      }
+    }
+  }, [tenzies]);
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        id: nanoid(),
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+      });
+    }
+    return newDice;
+  }
+
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((d) => (d.id === id ? { ...d, isHeld: !d.isHeld } : d))
+    );
+  }
+
+  const diceList = dice.map((element) => {
+    return (
+      <Dice
+        key={element.id}
+        id={element.id}
+        value={element.value}
+        isHeld={element.isHeld}
+        holdDice={() => holdDice(element.id)}
+      />
+    );
+  });
+  function generateDice() {
+    return {
+      id: nanoid(),
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+    };
+  }
+  function rollDice() {
+    if (tenzies) {
+      setDice((oldDice) => {
+        oldDice = allNewDice();
+        return oldDice;
+      });
+      setTenzies(false);
+      setTimes(0);
+    } else {
+      setDice((oldDice) =>
+        oldDice.map((d) => {
+          return d.isHeld ? d : generateDice();
+        })
+      );
+      setTimes(times + 1);
+    }
+  }
+
   return (
     <main>
-      <div className="dice-container">
-        <Dice value={1} className="dice"/>
-        <Dice value={2} className="dice"/>
-        <Dice value={3} className="dice"/>
-        <Dice value={4} className="dice"/>
-        <Dice value={5} className="dice"/>
-        <Dice value={6} className="dice"/>
-        <Dice value={7} className="dice"/>
-        <Dice value={8} className="dice"/>
-        <Dice value={9} className="dice"/>
-        <Dice value={10} className="dice"/>
+      {tenzies && <Confetti />}
+      <div className="title-container">
+        <h1 className="title">Tenzies</h1>
+        <h2> Roll Count: {times}</h2>
+        <h3> Best Count: {bestTime} </h3>
+        <p className="instructions">
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
       </div>
+      <div className="dice-container"> {diceList} </div>
+      <button onClick={rollDice} className="roll-dice">
+        {tenzies ? "New game" : "Roll"}{" "}
+      </button>
     </main>
   );
 }
--- src/Components/Dice.jsx
+++ src/Components/Dice.jsx
@@ -1,11 +1,22 @@
 import PropTypes from "prop-types";
-export default function Dice(props){
-    return(
-        <div className="dice">
-            <p> {props.value} </p>
-        </div>
-    )
+export default function Dice(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "#FFFFFF",
+  };
+  return (
+    <div
+      onClick={props.holdDice}
+      className="dice"
+      style={styles}
+    >
+      <p> {props.value} </p>
+    </div>
+  );
 }
-Dice.propTypes={
-    value:PropTypes.number
-}
+
+Dice.propTypes = {
+  id: PropTypes.string,
+  value: PropTypes.number,
+  holdDice: PropTypes.func,
+  isHeld: PropTypes.bool,
+};
